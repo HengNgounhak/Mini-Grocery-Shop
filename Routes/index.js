4cb91c1141b5a30cd6f37872bbb52998dc3ae9a8
Heng Ngounhak <hengngounhak@gmail.com>
2022-02-03 17:04:29 +0700

    fix filename

--- Routes/index.js
+++ Routes/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const homeController = require("../controllers/home");
-const authenController = require("../controllers/authentication");
-const adminPanelController = require("../Controllers/adminPanel");
-const purchaseController = require("../Controllers/purchases");
+const homeController = require("../Controllers/Home");
+const authenController = require("../Controllers/Authentication");
+const adminPanelController = require("../Controllers/AdminPanel");
+const purchaseController = require("../Controllers/Purchases");
 
 router.get('/', homeController.getHomePage);
 router.get('/product', homeController.getProductPage);
